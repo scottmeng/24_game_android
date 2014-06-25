4d7749d87c7c6d9883e3caa22473a2ce6f5b39b9
Meng Kaizhi <mengk@garena.com>
2014-06-25 19:25:36 +0800

    add drag event listener

--- app/src/main/java/kaizhi/play24/MainActivity.java
+++ app/src/main/java/kaizhi/play24/MainActivity.java
@@ -2,16 +2,31 @@ package kaizhi.play24;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnTouchListener, View.OnDragListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.txv_first).setOnTouchListener(this);
+        findViewById(R.id.txv_second).setOnTouchListener(this);
+        findViewById(R.id.txv_third).setOnTouchListener(this);
+        findViewById(R.id.txv_fourth).setOnTouchListener(this);
+
+        findViewById(R.id.txv_plus).setOnDragListener(this);
+        findViewById(R.id.txv_minus).setOnDragListener(this);
+        findViewById(R.id.txv_multiply).setOnDragListener(this);
+        findViewById(R.id.txv_divide).setOnDragListener(this);
     }
 
 
@@ -32,5 +47,54 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        final int dragEvent = event.getAction();
+        Log.i("test", "on drag action: " + dragEvent);
+        switch (dragEvent) {
+            case DragEvent.ACTION_DRAG_ENTERED:
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                break;
+            case DragEvent.ACTION_DROP:
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+                View view = (View) event.getLocalState();
+                view.setVisibility(View.VISIBLE);
+                break;
+        }
+
+        /*
+        if (event.getAction()==DragEvent.ACTION_DROP) {
+            View view = (View) event.getLocalState();
+            ViewGroup from = (ViewGroup) view.getParent();
+            from.removeView(view);
+            LinearLayout to = (LinearLayout) v;
+            to.addView(view);
+            view.setVisibility(View.VISIBLE);
+        }
+        */
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int motionEvent = event.getAction();
+        Log.i("test", "on touch action: " + motionEvent);
+        switch (motionEvent) {
+            case MotionEvent.ACTION_DOWN:
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+                v.startDrag(null, shadowBuilder, v, 0);
+                v.setVisibility(View.INVISIBLE);
+                break;
+            case MotionEvent.ACTION_UP:
+                v.setVisibility(View.VISIBLE);
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 }
